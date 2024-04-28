853ed6b8f62ddc31c553c8051798ad3be3d8fafa
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:47:06 -0400

    improve

--- libs/cli/langchain_cli/integration_template/scripts/lint_imports.sh
+++ libs/cli/langchain_cli/integration_template/scripts/lint_imports.sh
@@ -5,14 +5,15 @@ set -eu
 # Initialize a variable to keep track of errors
 errors=0
 
-# make sure not importing from langchain, langchain_experimental, or langchain_community
-git --no-pager grep '^from langchain\.' . && errors=$((errors+1))
-git --no-pager grep '^from langchain_experimental\.' . && errors=$((errors+1))
-git --no-pager grep '^from langchain_community\.' . && errors=$((errors+1))
-
-# Decide on an exit status based on the errors
-if [ "$errors" -gt 0 ]; then
-    exit 1
-else
-    exit 0
+# Check for imports from langchain, langchain_experimental, or langchain_community
+if git --no-pager grep -rE '^from (langchain|langchain_experimental|langchain_community)\.' . > /dev/null; then
+    errors=$((errors+1))
 fi
+
+# Exit with a status code of 1 if any errors were found
+if [ "$errors" -ne 0 ]; then
+    exit 1
+fi
+
+# Exit with a status code of 0 if no errors were found
+exit 0
