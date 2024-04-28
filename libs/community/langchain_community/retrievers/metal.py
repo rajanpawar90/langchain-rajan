38985600267570a19cdf4db30f237ab4244ced47
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:46:46 -0400

    improve

--- libs/community/langchain_community/retrievers/metal.py
+++ libs/community/langchain_community/retrievers/metal.py
@@ -1,29 +1,28 @@
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Optional
 
 from langchain_core.callbacks import CallbackManagerForRetrieverRun
 from langchain_core.documents import Document
-from langchain_core.pydantic_v1 import root_validator
+from langchain_core.exceptions import InvalidClientType
+from langchain_core.pydantic_v1 import BaseModel, root_validator
 from langchain_core.retrievers import BaseRetriever
-
+from metal_sdk.metal import Metal
 
 class MetalRetriever(BaseRetriever):
     """`Metal API` retriever."""
 
-    client: Any
+    client: Metal
     """The Metal client to use."""
-    params: Optional[dict] = None
+    params: Optional[Dict[str, Any]] = None
     """The parameters to pass to the Metal client."""
 
     @root_validator(pre=True)
     def validate_client(cls, values: dict) -> dict:
         """Validate that the client is of the correct type."""
-        from metal_sdk.metal import Metal
-
         if "client" in values:
             client = values["client"]
             if not isinstance(client, Metal):
-                raise ValueError(
-                    "Got unexpected client, should be of type metal_sdk.metal.Metal. "
+                raise InvalidClientType(
+                    f"Got unexpected client, should be of type Metal. "
                     f"Instead, got {type(client)}"
                 )
 
