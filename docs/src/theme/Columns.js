45c2ecc729ed412cce4cc17cf141627280df4f20
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:47:05 -0400

    improve

--- docs/src/theme/Columns.js
+++ docs/src/theme/Columns.js
@@ -1,17 +1,26 @@
 import React from "react";
 
-export function ColumnContainer({children}) {
-    return (
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
-            {children}
-        </div>
-    )
+export interface ColumnContainerProps {
+  children: React.ReactNode;
 }
 
-export function Column({children}) {
-    return (
-        <div style={{ flex: "1 0 300px", padding: "10px", overflowX: "clip", zoom: '80%' }}>
-            {children}
-        </div>
-    )
+export interface ColumnProps {
+  children: React.ReactNode;
+  className?: string;
 }
+
+export const ColumnContainer: React.FC<ColumnContainerProps> = ({ children }) => {
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {React.Children.map(children, (child, index) => (
+        <div key={index} style={{ flex: "1 0 300px", padding: "10px", overflowX: "clip", zoom: "80%" }}>
+          {child}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const Column: React.FC<ColumnProps> = ({ children, className }) => {
+  return <div className={className} style={{ flex: "1 0 150px", padding: "10px", overflowX: "clip", zoom: "80%" }}>{children}</div>;
+};
