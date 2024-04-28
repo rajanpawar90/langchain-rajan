c93c513f2fdf070967193d03e219b99174eacca7
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:46:40 -0400

    improve

--- libs/community/langchain_community/document_loaders/open_city_data.py
+++ libs/community/langchain_community/document_loaders/open_city_data.py
@@ -1,14 +1,14 @@
+import warnings
 from typing import Iterator
 
+from sodapy import Socrata
 from langchain_core.documents import Document
-
 from langchain_community.document_loaders.base import BaseLoader
-
 
 class OpenCityDataLoader(BaseLoader):
     """Load from `Open City`."""
 
-    def __init__(self, city_id: str, dataset_id: str, limit: int):
+    def __init__(self, city_id: str, dataset_id: str, limit: int = 1000):
         """Initialize with dataset_id.
         Example: https://dev.socrata.com/foundry/data.sfgov.org/vw6y-z8j6
         e.g., city_id = data.sfgov.org
@@ -17,7 +17,7 @@ class OpenCityDataLoader(BaseLoader):
         Args:
             city_id: The Open City city identifier.
             dataset_id: The Open City dataset identifier.
-            limit: The maximum number of documents to load.
+            limit: The maximum number of documents to load. Defaults to 1000.
         """
         self.city_id = city_id
         self.dataset_id = dataset_id
@@ -26,14 +26,21 @@ class OpenCityDataLoader(BaseLoader):
     def lazy_load(self) -> Iterator[Document]:
         """Lazy load records."""
 
-        from sodapy import Socrata
+        warnings.filterwarnings("ignore", category=UserWarning)
 
-        client = Socrata(self.city_id, None)
-        results = client.get(self.dataset_id, limit=self.limit)
-        for record in results:
-            yield Document(
-                page_content=str(record),
-                metadata={
-                    "source": self.city_id + "_" + self.dataset_id,
-                },
-            )
+        try:
+            with warnings.catch_warnings():
+                client = Socrata(self.city_id, None)
+                results = client.get(self.dataset_id, limit=self.limit)
+
+            for record in results:
+                yield Document(
+                    page_content=str(record),
+                    metadata={
+                        "source": self.city_id + "_" + self.dataset_id,
+                    },
+                )
+
+        except Exception as e:
+            print(f"Error fetching data from OpenCityDataLoader: {e}")
+            raise
