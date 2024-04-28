1df69f8c0259f659070ca61fd01e4cf281d76034
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:47:18 -0400

    improve

--- libs/cli/tests/test_utils.py
+++ libs/cli/tests/test_utils.py
@@ -10,128 +10,84 @@ from langchain_cli.constants import (
 from langchain_cli.utils.git import DependencySource, parse_dependency_string
 
 
-def _assert_dependency_equals(
-    dep: DependencySource,
-    *,
-    git: Optional[str] = None,
-    ref: Optional[str] = None,
-    subdirectory: Optional[str] = None,
-    event_metadata: Optional[Dict] = None,
-) -> None:
-    assert dep["git"] == git
-    assert dep["ref"] == ref
-    assert dep["subdirectory"] == subdirectory
-    if event_metadata is not None:
-        assert dep["event_metadata"] == event_metadata
+def _assert_dependency_equals(dep: DependencySource, expected: DependencySource) -> None:
+    assert dep == expected
 
 
 def test_dependency_string() -> None:
-    _assert_dependency_equals(
-        parse_dependency_string(
-            "git+ssh://git@github.com/efriis/myrepo.git", None, None, None
+    test_cases = [
+        (
+            "git+ssh://git@github.com/efriis/myrepo.git",
+            {"git": "ssh://git@github.com/efriis/myrepo.git", "ref": None, "subdirectory": None},
         ),
-        git="ssh://git@github.com/efriis/myrepo.git",
-        ref=None,
-        subdirectory=None,
-    )
-
-    _assert_dependency_equals(
-        parse_dependency_string(
+        (
             "git+https://github.com/efriis/myrepo.git#subdirectory=src",
-            None,
-            None,
-            None,
+            {"git": "https://github.com/efriis/myrepo.git", "subdirectory": "src", "ref": None},
         ),
-        git="https://github.com/efriis/myrepo.git",
-        subdirectory="src",
-        ref=None,
-    )
-
-    _assert_dependency_equals(
-        parse_dependency_string(
-            "git+ssh://git@github.com:efriis/myrepo.git#develop", None, None, None
+        (
+            "git+ssh://git@github.com:efriis/myrepo.git#develop",
+            {"git": "ssh://git@github.com:efriis/myrepo.git", "ref": "develop", "subdirectory": None},
         ),
-        git="ssh://git@github.com:efriis/myrepo.git",
-        ref="develop",
-        subdirectory=None,
-    )
-
-    # also support a slash in ssh
-    _assert_dependency_equals(
-        parse_dependency_string(
-            "git+ssh://git@github.com/efriis/myrepo.git#develop", None, None, None
+        (
+            "git+ssh://git@github.com/efriis/myrepo.git#develop",
+            {"git": "ssh://git@github.com/efriis/myrepo.git", "ref": "develop", "subdirectory": None},
         ),
-        git="ssh://git@github.com/efriis/myrepo.git",
-        ref="develop",
-        subdirectory=None,
-    )
-
-    # looks like poetry supports both an @ and a #
-    _assert_dependency_equals(
-        parse_dependency_string(
-            "git+ssh://git@github.com:efriis/myrepo.git@develop", None, None, None
+        (
+            "git+ssh://git@github.com:efriis/myrepo.git@develop",
+            {"git": "ssh://git@github.com:efriis/myrepo.git", "ref": "develop", "subdirectory": None},
         ),
-        git="ssh://git@github.com:efriis/myrepo.git",
-        ref="develop",
-        subdirectory=None,
-    )
+        (
+            "simple-pirate",
+            {
+                "git": DEFAULT_GIT_REPO,
+                "subdirectory": f"{DEFAULT_GIT_SUBDIRECTORY}/simple-pirate",
+                "ref": DEFAULT_GIT_REF,
+            },
+        ),
+    ]
 
-    _assert_dependency_equals(
-        parse_dependency_string("simple-pirate", None, None, None),
-        git=DEFAULT_GIT_REPO,
-        subdirectory=f"{DEFAULT_GIT_SUBDIRECTORY}/simple-pirate",
-        ref=DEFAULT_GIT_REF,
-    )
+    for input_string, expected_dep in test_cases:
+        parsed_dep = parse_dependency_string(input_string, None, None, None)
+        _assert_dependency_equals(parsed_dep, expected_dep)
 
 
 def test_dependency_string_both() -> None:
-    _assert_dependency_equals(
-        parse_dependency_string(
+    test_cases = [
+        (
             "git+https://github.com/efriis/myrepo.git@branch#subdirectory=src",
-            None,
-            None,
-            None,
+            {
+                "git": "https://github.com/efriis/myrepo.git",
+                "subdirectory": "src",
+                "ref": "branch",
+            },
         ),
-        git="https://github.com/efriis/myrepo.git",
-        subdirectory="src",
-        ref="branch",
-    )
+    ]
+
+    for input_string, expected_dep in test_cases:
+        parsed_dep = parse_dependency_string(input_string, None, None, None)
+        _assert_dependency_equals(parsed_dep, expected_dep)
 
 
 def test_dependency_string_invalids() -> None:
-    # expect error for wrong order
-    with pytest.raises(ValueError):
-        parse_dependency_string(
-            "git+https://github.com/efriis/myrepo.git#subdirectory=src@branch",
-            None,
-            None,
-            None,
-        )
-    # expect error for @subdirectory
+    with pytest.raises(ValueError, match="Unexpected order of git dependency components"):
+        parse_dependency_string("git+https://github.com/efriis/myrepo.git#subdirectory=src@branch", None, None, None)
+
+    with pytest.raises(ValueError, match="Unexpected format of git dependency component"):
+        parse_dependency_string("git+https://github.com/efriis/myrepo.git@subdirectory=src", None, None, None)
 
 
 def test_dependency_string_edge_case() -> None:
-    # weird unsolvable edge case of
-    # git+ssh://a@b
-    # this could be a ssh dep with user=a, and default ref
-    # or a ssh dep at a with ref=b.
-    # in this case, assume the first case (be greedy with the '@')
-    _assert_dependency_equals(
-        parse_dependency_string("git+ssh://a@b", None, None, None),
-        git="ssh://a@b",
-        subdirectory=None,
-        ref=None,
-    )
-
-    # weird one that is actually valid
-    _assert_dependency_equals(
-        parse_dependency_string(
-            "git+https://github.com/efriis/myrepo.git@subdirectory=src",
-            None,
-            None,
-            None,
+    test_cases = [
+        (
+            "git+ssh://a@b",
+            {"git": "ssh://a@b", "subdirectory": None, "ref": None},
         ),
-        git="https://github.com/efriis/myrepo.git",
-        subdirectory=None,
-        ref="subdirectory=src",
-    )
+        (
+            "git+ssh://a@b.com/path",
+            {"git": "ssh://a@b.com/path", "subdirectory": None, "ref": None},
+        ),
+    ]
+
+    for input_string, expected_dep in test_cases:
+        parsed_dep = parse_dependency_string(input_string, None, None, None)
+        _assert_dependency_equals(parsed_dep, expected_dep)
