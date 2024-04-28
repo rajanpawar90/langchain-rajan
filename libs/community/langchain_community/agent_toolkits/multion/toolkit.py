192fee65a156723cc94f15710580068d0ba36c08
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:47:02 -0400

    improve

--- libs/community/langchain_community/agent_toolkits/multion/toolkit.py
+++ libs/community/langchain_community/agent_toolkits/multion/toolkit.py
@@ -1,4 +1,4 @@
-"""MultiOn agent."""
+"""MultiOn agent toolkit."""
 from __future__ import annotations
 
 from typing import List
@@ -10,14 +10,14 @@ from langchain_community.tools.multion.create_session import MultionCreateSessio
 from langchain_community.tools.multion.update_session import MultionUpdateSession
 
 
-class MultionToolkit(BaseToolkit):
-    """Toolkit for interacting with the Browser Agent.
+class MultiOnToolkit(BaseToolkit):
+    """Toolkit for interacting with the MultiOn API.
 
     **Security Note**: This toolkit contains tools that interact with the
-        user's browser via the multion API which grants an agent
+        user's browser via the MultiOn API which grants an agent
         access to the user's browser.
 
-        Please review the documentation for the multion API to understand
+        Please review the documentation for the MultiOn API to understand
         the security implications of using this toolkit.
 
         See https://python.langchain.com/docs/security for more information.
@@ -28,6 +28,14 @@ class MultionToolkit(BaseToolkit):
 
         arbitrary_types_allowed = True
 
+    def __init__(self):
+        """Initialize the toolkit with tools."""
+        self.tools = [
+            MultionCreateSession(),
+            MultionUpdateSession(),
+            MultionCloseSession(),
+        ]
+
     def get_tools(self) -> List[BaseTool]:
         """Get the tools in the toolkit."""
-        return [MultionCreateSession(), MultionUpdateSession(), MultionCloseSession()]
+        return self.tools
