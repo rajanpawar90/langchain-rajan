50819b375f976ee77b7ed2d3ef6b3175749d90b3
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:47:18 -0400

    improve

--- libs/community/langchain_community/document_loaders/tsv.py
+++ libs/community/langchain_community/document_loaders/tsv.py
@@ -1,29 +1,13 @@
 from pathlib import Path
 from typing import Any, List, Union
 
-from langchain_community.document_loaders.unstructured import (
-    UnstructuredFileLoader,
-    validate_unstructured_version,
-)
-
-
 class UnstructuredTSVLoader(UnstructuredFileLoader):
     """Load `TSV` files using `Unstructured`.
 
-    Like other
-    Unstructured loaders, UnstructuredTSVLoader can be used in both
-    "single" and "elements" mode. If you use the loader in "elements"
-    mode, the TSV file will be a single Unstructured Table element.
-    If you use the loader in "elements" mode, an HTML representation
-    of the table will be available in the "text_as_html" key in the
+    This loader can be used in both "single" and "elements" mode. If you use the loader in "elements"
+    mode, the TSV file will be a single Unstructured Table element. If you use the loader in "elements"
+    mode, an HTML representation of the table will be available in the "text_as_html" key in the
     document metadata.
-
-    Examples
-    --------
-    from langchain_community.document_loaders.tsv import UnstructuredTSVLoader
-
-    loader = UnstructuredTSVLoader("stanley-cups.tsv", mode="elements")
-    docs = loader.load()
     """
 
     def __init__(
@@ -35,7 +19,10 @@ class UnstructuredTSVLoader(UnstructuredFileLoader):
         validate_unstructured_version(min_unstructured_version="0.7.6")
         super().__init__(file_path=file_path, mode=mode, **unstructured_kwargs)
 
-    def _get_elements(self) -> List:
-        from unstructured.partition.tsv import partition_tsv
+    def _get_elements(self) -> List[Any]:
+        from unstructured.partition.tsv import partition_tsv  # import here to avoid unnecessary imports
 
-        return partition_tsv(filename=self.file_path, **self.unstructured_kwargs)
+        try:
+            return partition_tsv(filename=self.file_path, **self.unstructured_kwargs)
+        except Exception as e:
+            raise Exception(f"Error while partitioning TSV file: {e}")
