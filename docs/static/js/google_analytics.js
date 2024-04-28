69adb363fc669789c916f0dd7517cc1fa21a1585
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:47:29 -0400

    improve

--- docs/static/js/google_analytics.js
+++ docs/static/js/google_analytics.js
@@ -1,5 +1,2 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+// Create a namespace for the tracking function to avoid conflicts with other scripts
 
-gtag('config', 'G-9B66JQQH2F');
