d22663178e811f268686d770e5c90ebac7cd7264
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:47:40 -0400

    improve

--- libs/community/langchain_community/document_loaders/college_confidential.py
+++ libs/community/langchain_community/document_loaders/college_confidential.py
@@ -1,16 +1,25 @@
 from typing import List
 
+from bs4 import BeautifulSoup
+import requests
 from langchain_core.documents import Document
 
-from langchain_community.document_loaders.web_base import WebBaseLoader
-
-
-class CollegeConfidentialLoader(WebBaseLoader):
+class CollegeConfidentialLoader:
     """Load `College Confidential` webpages."""
 
+    def __init__(self, web_path: str):
+        self.web_path = web_path
+
+    def scrape(self) -> BeautifulSoup:
+        """Scrape webpage and return BeautifulSoup object."""
+        response = requests.get(self.web_path)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
+
     def load(self) -> List[Document]:
-        """Load webpages as Documents."""
+        """Load webpage as Document."""
         soup = self.scrape()
-        text = soup.select_one("main[class='skin-handler']").text
+        text = soup.select_one("main[class='skin-handler']").get_text()
         metadata = {"source": self.web_path}
         return [Document(page_content=text, metadata=metadata)]
