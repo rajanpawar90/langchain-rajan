fce01089faaddce142e2300680a2e99920e947d4
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:47:01 -0400

    improve

--- libs/community/langchain_community/document_loaders/onedrive_file.py
+++ libs/community/langchain_community/document_loaders/onedrive_file.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-import tempfile
+import pathlib
+import shutil
 from typing import TYPE_CHECKING, List
 
 from langchain_core.documents import Document
@@ -16,20 +17,25 @@ CHUNK_SIZE = 1024 * 1024 * 5
 
 
 class OneDriveFileLoader(BaseLoader, BaseModel):
-    """Load a file from `Microsoft OneDrive`."""
+    """Load a file from Microsoft OneDrive."""
 
     file: File = Field(...)
     """The file to load."""
 
     class Config:
         arbitrary_types_allowed = True
-        """Allow arbitrary types. This is needed for the File type. Default is True.
-         See https://pydantic-docs.helpmanual.io/usage/types/#arbitrary-types-allowed"""
 
     def load(self) -> List[Document]:
-        """Load Documents"""
-        with tempfile.TemporaryDirectory() as temp_dir:
-            file_path = f"{temp_dir}/{self.file.name}"
+        """Load documents from the file.
+
+        Returns:
+            List[Document]: The loaded documents.
+        """
+        temp_dir = pathlib.Path(tempfile.TemporaryDirectory().name)
+        file_path = temp_dir / self.file.name
+        try:
             self.file.download(to_path=temp_dir, chunk_size=CHUNK_SIZE)
             loader = UnstructuredFileLoader(file_path)
             return loader.load()
+        finally:
+            shutil.rmtree(temp_dir)
