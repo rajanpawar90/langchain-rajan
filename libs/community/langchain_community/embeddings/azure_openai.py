72c7b8d85a195ceaa2526bb5dd120e71d9b6eca2
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:46:52 -0400

    improve

--- libs/community/langchain_community/embeddings/azure_openai.py
+++ libs/community/langchain_community/embeddings/azure_openai.py
@@ -11,7 +11,6 @@ from langchain_core.pydantic_v1 import Field, root_validator
 from langchain_core.utils import get_from_dict_or_env
 
 from langchain_community.embeddings.openai import OpenAIEmbeddings
-from langchain_community.utils.openai import is_openai_v1
 
 
 @deprecated(
@@ -23,90 +22,35 @@ class AzureOpenAIEmbeddings(OpenAIEmbeddings):
     """`Azure OpenAI` Embeddings API."""
 
     azure_endpoint: Union[str, None] = None
-    """Your Azure endpoint, including the resource.
-
-        Automatically inferred from env var `AZURE_OPENAI_ENDPOINT` if not provided.
-        
-        Example: `https://example-resource.azure.openai.com/`
-    """
     deployment: Optional[str] = Field(default=None, alias="azure_deployment")
-    """A model deployment. 
-
-        If given sets the base client URL to include `/deployments/{azure_deployment}`.
-        Note: this means you won't be able to use non-deployment endpoints.
-    """
     openai_api_key: Union[str, None] = Field(default=None, alias="api_key")
-    """Automatically inferred from env var `AZURE_OPENAI_API_KEY` if not provided."""
     azure_ad_token: Union[str, None] = None
-    """Your Azure Active Directory token.
-
-        Automatically inferred from env var `AZURE_OPENAI_AD_TOKEN` if not provided.
-
-        For more: 
-        https://www.microsoft.com/en-us/security/business/identity-access/microsoft-entra-id.
-    """  # noqa: E501
     azure_ad_token_provider: Union[Callable[[], str], None] = None
-    """A function that returns an Azure Active Directory token.
-
-        Will be invoked on every request.
-    """
     openai_api_version: Optional[str] = Field(default=None, alias="api_version")
-    """Automatically inferred from env var `OPENAI_API_VERSION` if not provided."""
     validate_base_url: bool = True
 
     @root_validator()
-    def validate_environment(cls, values: Dict) -> Dict:
+    def validate_environment(self, values: Dict) -> Dict:
         """Validate that api key and python package exists in environment."""
-        # Check OPENAI_KEY for backwards compatibility.
-        # TODO: Remove OPENAI_API_KEY support to avoid possible conflict when using
-        # other forms of azure credentials.
-        values["openai_api_key"] = (
-            values["openai_api_key"]
-            or os.getenv("AZURE_OPENAI_API_KEY")
-            or os.getenv("OPENAI_API_KEY")
-        )
-        values["openai_api_base"] = values["openai_api_base"] or os.getenv(
-            "OPENAI_API_BASE"
-        )
-        values["openai_api_version"] = values["openai_api_version"] or os.getenv(
-            "OPENAI_API_VERSION", default="2023-05-15"
-        )
-        values["openai_api_type"] = get_from_dict_or_env(
-            values, "openai_api_type", "OPENAI_API_TYPE", default="azure"
-        )
-        values["openai_organization"] = (
-            values["openai_organization"]
-            or os.getenv("OPENAI_ORG_ID")
-            or os.getenv("OPENAI_ORGANIZATION")
-        )
-        values["openai_proxy"] = get_from_dict_or_env(
-            values,
-            "openai_proxy",
-            "OPENAI_PROXY",
-            default="",
-        )
-        values["azure_endpoint"] = values["azure_endpoint"] or os.getenv(
-            "AZURE_OPENAI_ENDPOINT"
-        )
-        values["azure_ad_token"] = values["azure_ad_token"] or os.getenv(
-            "AZURE_OPENAI_AD_TOKEN"
-        )
-        # Azure OpenAI embedding models allow a maximum of 16 texts
-        # at a time in each batch
-        # See: https://learn.microsoft.com/en-us/azure/ai-services/openai/reference#embeddings
-        values["chunk_size"] = min(values["chunk_size"], 16)
+        values["openai_api_key"] = values.get("openai_api_key", os.getenv("AZURE_OPENAI_API_KEY"))
+        values["openai_api_base"] = values.get("openai_api_base", os.getenv("OPENAI_API_BASE"))
+        values["openai_api_version"] = values.get("openai_api_version", os.getenv("OPENAI_API_VERSION", default="2023-05-15"))  # noqa: E501
+        values["openai_organization"] = values.get("openai_organization", os.getenv("OPENAI_ORG_ID", os.getenv("OPENAI_ORGANIZATION")))  # noqa: E501
+        values["openai_proxy"] = get_from_dict_or_env(values, "openai_proxy", "OPENAI_PROXY", default="")  # noqa: E501
+        values["azure_endpoint"] = values.get("azure_endpoint", os.getenv("AZURE_OPENAI_ENDPOINT"))
+        values["azure_ad_token"] = values.get("azure_ad_token", os.getenv("AZURE_OPENAI_AD_TOKEN"))
+        values["chunk_size"] = min(values.get("chunk_size", 16), 16)
+
         try:
             import openai
-
         except ImportError:
             raise ImportError(
                 "Could not import openai python package. "
                 "Please install it with `pip install openai`."
             )
+
         if is_openai_v1():
-            # For backwards compatibility. Before openai v1, no distinction was made
-            # between azure_endpoint and base_url (openai_api_base).
-            openai_api_base = values["openai_api_base"]
+            openai_api_base = values.get("openai_api_base")
             if openai_api_base and values["validate_base_url"]:
                 if "/openai" not in openai_api_base:
                     values["openai_api_base"] += "/openai"
@@ -152,10 +96,11 @@ class AzureOpenAIEmbeddings(OpenAIEmbeddings):
                 "default_query": values["default_query"],
                 "http_client": values["http_client"],
             }
-            values["client"] = openai.AzureOpenAI(**client_params).embeddings
-            values["async_client"] = openai.AsyncAzureOpenAI(**client_params).embeddings
+            values["client"] = openai.AzureOpenAI(**client_params).embeddings  # type: ignore  # noqa: E501
+            values["async_client"] = openai.AsyncAzureOpenAI(**client_params).embeddings  # type: ignore  # noqa: E501
         else:
             values["client"] = openai.Embedding
+
         return values
 
     @property
