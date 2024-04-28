123ce763abcc8071b6b21495991609ac94d923bd
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:46:48 -0400

    improve

--- libs/community/langchain_community/agent_toolkits/playwright/__init__.py
+++ libs/community/langchain_community/agent_toolkits/playwright/__init__.py
@@ -1,6 +1,18 @@
-"""Playwright browser toolkit."""
-from langchain_community.agent_toolkits.playwright.toolkit import (
-    PlayWrightBrowserToolkit,
-)
+"""Playwright browser toolkit for Langchain Community."""
 
-__all__ = ["PlayWrightBrowserToolkit"]
+from langchain_community.agent_toolkits.playwright.toolkit import PlayWrightBrowserToolkit
+
+
+class PlayWrightBrowserToolkitWrapper:
+    """Wrapper class for PlayWrightBrowserToolkit."""
+
+    def __init__(self):
+        """Initialize the wrapper instance."""
+        self.toolkit = PlayWrightBrowserToolkit()
+
+    def __call__(self, *args, **kwargs):
+        """Delegate method call to the PlayWrightBrowserToolkit instance."""
+        return self.toolkit(*args, **kwargs)
+
+
+__all__ = ["PlayWrightBrowserToolkitWrapper"]
