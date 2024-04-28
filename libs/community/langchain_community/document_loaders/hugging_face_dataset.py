657007ccc7ae1b2d65336886e8cc5524f033cb22
Rajan Pawar <rajanpawar90@gmail.com>
2024-04-28 14:47:09 -0400

    improve

--- libs/community/langchain_community/document_loaders/hugging_face_dataset.py
+++ libs/community/langchain_community/document_loaders/hugging_face_dataset.py
@@ -1,59 +1,42 @@
 import json
-from typing import Iterator, Mapping, Optional, Sequence, Union
+import itertools
+from functools import lru_cache
+from typing import Any, Iterator, Literal, Mapping, Optional, Sequence, Union
 
+import datasets
+from dataclasses import dataclass
+from itertools import chain
 from langchain_core.documents import Document
 
-from langchain_community.document_loaders.base import BaseLoader
+@dataclass
+class HuggingFaceDatasetLoaderConfig:
+    path: str
+    page_content_column: str = "text"
+    name: Optional[str] = None
+    data_dir: Optional[str] = None
+    data_files: Optional[
+        Union[str, Sequence[str], Mapping[str, Union[str, Sequence[str]]]]
+    ] = None
+    cache_dir: Optional[str] = None
+    keep_in_memory: Optional[bool] = None
+    save_infos: bool = False
+    use_auth_token: Optional[Union[bool, str]] = None
+    num_proc: Optional[int] = None
 
 
 class HuggingFaceDatasetLoader(BaseLoader):
     """Load from `Hugging Face Hub` datasets."""
 
-    def __init__(
-        self,
-        path: str,
-        page_content_column: str = "text",
-        name: Optional[str] = None,
-        data_dir: Optional[str] = None,
-        data_files: Optional[
-            Union[str, Sequence[str], Mapping[str, Union[str, Sequence[str]]]]
-        ] = None,
-        cache_dir: Optional[str] = None,
-        keep_in_memory: Optional[bool] = None,
-        save_infos: bool = False,
-        use_auth_token: Optional[Union[bool, str]] = None,
-        num_proc: Optional[int] = None,
-    ):
-        """Initialize the HuggingFaceDatasetLoader.
+    def __init__(self, config: HuggingFaceDatasetLoaderConfig):
+        self.config = config
 
-        Args:
-            path: Path or name of the dataset.
-            page_content_column: Page content column name. Default is "text".
-            name: Name of the dataset configuration.
-            data_dir: Data directory of the dataset configuration.
-            data_files: Path(s) to source data file(s).
-            cache_dir: Directory to read/write data.
-            keep_in_memory: Whether to copy the dataset in-memory.
-            save_infos: Save the dataset information (checksums/size/splits/...).
-              Default is False.
-            use_auth_token: Bearer token for remote files on the Dataset Hub.
-            num_proc: Number of processes.
-        """
+    @lru_cache(maxsize=None)
+    def _parse_obj(self, page_content: Union[str, object]) -> str:
+        if isinstance(page_content, object):
+            return json.dumps(page_content)
+        return page_content
 
-        self.path = path
-        self.page_content_column = page_content_column
-        self.name = name
-        self.data_dir = data_dir
-        self.data_files = data_files
-        self.cache_dir = cache_dir
-        self.keep_in_memory = keep_in_memory
-        self.save_infos = save_infos
-        self.use_auth_token = use_auth_token
-        self.num_proc = num_proc
-
-    def lazy_load(
-        self,
-    ) -> Iterator[Document]:
+    def lazy_load(self) -> Iterator[Document]:
         """Load documents lazily."""
         try:
             from datasets import load_dataset
@@ -64,27 +47,22 @@ class HuggingFaceDatasetLoader(BaseLoader):
             )
 
         dataset = load_dataset(
-            path=self.path,
-            name=self.name,
-            data_dir=self.data_dir,
-            data_files=self.data_files,
-            cache_dir=self.cache_dir,
-            keep_in_memory=self.keep_in_memory,
-            save_infos=self.save_infos,
-            use_auth_token=self.use_auth_token,
-            num_proc=self.num_proc,
+            path=self.config.path,
+            name=self.config.name,
+            data_dir=self.config.data_dir,
+            data_files=self.config.data_files,
+            cache_dir=self.config.cache_dir,
+            keep_in_memory=self.config.keep_in_memory,
+            save_infos=self.config.save_infos,
+            use_auth_token=self.config.use_auth_token,
+            num_proc=self.config.num_proc,
         )
 
         yield from (
             Document(
-                page_content=self.parse_obj(row.pop(self.page_content_column)),
+                page_content=self._parse_obj(row.pop(self.config.page_content_column)),
                 metadata=row,
             )
             for key in dataset.keys()
-            for row in dataset[key]
+            for row in dataset[key].map(self._parse_obj, batched=True)
         )
-
-    def parse_obj(self, page_content: Union[str, object]) -> str:
-        if isinstance(page_content, object):
-            return json.dumps(page_content)
-        return page_content
